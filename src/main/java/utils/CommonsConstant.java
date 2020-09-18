043f28ffd2ddcb985d34c7540c7edc7523cdf7de
Choiyounho <maxosa78@gmail.com>
2020-09-18 14:15:00 +0900

    [refactor] : 상수클래스 수정

--- src/main/java/utils/CommonsConstant.java
+++ src/main/java/utils/CommonsConstant.java
@@ -5,9 +5,9 @@ public class CommonsConstant {
     private CommonsConstant(){
     }
 
-    public static final String PATHNAME = "results/";
+    public static final String PATHNAME = "output/";
 
     public static final String IMAGE_URL = "https://mblogthumb-phinf.pstatic.net/MjAxOTEyMTVfMjc4/MDAxNTc2NDE0MTAwNjg1.cp_9N4gi8GOe7idQjx6pC1LUhK9EqpIs9uArKqZq6iUg.1vF6bTjG3vJW4mb_WagZ5gh0gfwjoo2bznBTEs-tyXkg.JPEG.nilsine11202/github.jpg?type=w800";
-    public static final String IMAGENAME = "results/github-octocat.png";
+    public static final String IMAGENAME = "input/github-octocat.png";
 
 }
