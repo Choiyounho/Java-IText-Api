e953f7d27784dee0cf1541201862ffd92038e175
Choiyounho <maxosa78@gmail.com>
2020-09-19 04:00:19 +0900

    [feat] : 엑셀데이터를 pdf 데이터로 변환하는 클래스 구현

---
+++ src/main/java/ExcelVo.java
@@ -0,0 +1,63 @@
+public class ExcelVo {
+
+    private String title;
+    private String author;
+    private String company;
+    private String isbn;
+    private String imageUrl;
+
+    public ExcelVo(String title, String author, String company, String isbn, String imageUrl) {
+        super();
+        this.title = title;
+        this.author = author;
+        this.company = company;
+        this.isbn = isbn;
+        this.imageUrl = imageUrl;
+    }
+
+    public ExcelVo(String title, String author, String company) {
+        this.title = title;
+        this.author = author;
+        this.company = company;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getImageUrl() {
+        return "input/" + imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "console.domain.ExcelVo{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", company='" + company + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", imageurl='" + imageUrl + '\'' +
+                '}';
+    }
+
+}
